df22d74784e7193d754b773bd1bf68eaa8307a9a
Fil Bryan <filbryancegos@gmail.com>
2019-06-25 18:27:17 +0800

    customsliderangularjs

---
+++ js/app.js
@@ -0,0 +1,69 @@
+var sliderApp=angular.module('sliderApp',['ngAnimate']);
+
+sliderApp.controller('SliderController', function($scope) {
+    $scope.images = [
+			{src:'img1.png',title:'Pic 1'},
+			{src:'img2.jpg',title:'Pic 2'},
+			{src:'img3.jpg',title:'Pic 3'},
+			{src:'img4.png',title:'Pic 4'},
+			{src:'img5.png',title:'Pic 5'}
+		]; 
+});
+ 
+sliderApp.directive('slider', function ($timeout) {
+  return {
+		restrict: 'AE',
+		replace: true,
+		scope:{
+			images: '='
+		},
+		link: function (scope, elem, attrs) {
+
+			scope.currentIndex = 0;
+
+			scope.next = function(){
+				if (scope.currentIndex < scope.images.length -1) {
+					scope.currentIndex++;
+				} else {
+					scope.currentIndex = 0;
+				}
+			};
+
+			scope.prev = function(){
+				if (scope.currentIndex > 0) {
+					scope.currentIndex--;
+				} else {
+					scope.images.length;
+				}
+			};
+
+			scope.$watch('currentIndex',function(){
+				scope.images.forEach(function(image){
+					image.visible = false;
+				});
+				scope.images[scope.currentIndex].visible = true;
+			});
+
+			/* Start: For Automatic slideshow*/
+
+			var timer;
+
+			var sliderFunc = function(){
+				timer = $timeout(function(){
+					scope.next();
+					timer = $timeout(sliderFunc,5000);
+				},5000);
+			};
+
+			sliderFunc();
+
+			scope.$on('$destroy',function(){
+				$timeout.cancel(timer);
+			});
+
+		/* End : For Automatic slideshow*/
+
+		},
+		templateUrl:'templates/templateurl.html'
+  }
+});
